e4ba880b9835c8c7f97696aa76091c9d56e1611d
EXL <exlmotodev@gmail.com>
2018-10-23 18:58:28 +0000

    Implement game/mod list

--- Xash3DLauncher.cpp
+++ Xash3DLauncher.cpp
@@ -8,14 +8,19 @@
 
 #include <Rect.h>
 #include <String.h>
+#include <StringList.h>
 #include <StringView.h>
 #include <CheckBox.h>
 #include <TextControl.h>
+#include <ListView.h>
+#include <ScrollView.h>
 #include <Message.h>
 #include <Font.h>
 #include <GroupLayout.h>
 #include <LayoutBuilder.h>
 #include <InterfaceDefs.h>
+#include <Directory.h>
+#include <Entry.h>
 
 #include <Catalog.h>
 
@@ -38,6 +43,7 @@
 #define MIRROR_PATH_OPT                "XASH3D_MIRRORDIR"
 #define GAME_OPT                       "XASH3D_GAME"
 #define EXTRAS_DIR_NAME                "/extras"
+#define VALVE_DIR_NAME                 "valve"
 
 // Various Strings
 #define L_ABOUT_STRING                 B_TRANSLATE("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do " \
@@ -62,12 +68,15 @@
 #define L_EXTRA_BUTTON_BROWSE          B_TRANSLATE("...")
 #define L_EXTRA_BUTTON_BROWSE_TOOLTIP  B_TRANSLATE("Click to open the file dialog.")
 #define L_ADDITIONAL_FILE_PANEL_TITLE  B_TRANSLATE("Please choose a libraries folder")
+#define L_ERROR_NO_VALVE_CATALOG       B_TRANSLATE("Game files directory does not contain the \"valve\" catalog.")
 #define O_CHECKBOX_OPTION              "checkBoxOption"
-
 #define O_ABOUT_LINK                   "aboutLink"
 #define O_ABOUT_LINK_DESC              "aboutLinkDesc"
 #define O_DATA_LINK                    "dataLink"
 #define O_DATA_LINK_DESC               "dataLinkDesc"
+#define O_VIEW_SCROLL                  "viewScroll"
+#define G_GAMES_LIST_HEIGHT            100.0f
+#define G_MAX_GAME_NAME_LENGTH         50
 
 class Xash3DAboutWindow : public BeAboutWindow
 {
@@ -98,15 +107,81 @@ class Xash3DGameLauncher : public BeLauncherBase
 	{
 		MSG_CHECKBOX_STATE_CHANGED              = 'chks',
 		MSG_ADDITIONAL_BROWSE_BUTTON_CLICKED    = 'abtc',
-		MSG_ADDITIONAL_FILE_PANEL_FILE_SELECTED = 'afps'
+		MSG_ADDITIONAL_FILE_PANEL_FILE_SELECTED = 'afps',
+		MSG_SELECTED_SOME_GAME                  = 'ssgl'
 	};
+
+	BString fSelectedGame;
 
 	BCheckBox *fCheckBoxOption;
 	BTextControl *fAdditionalTextControl;
 	BButton *fAdditionalBrowseButton;
+	BListView *fGamesList;
 
 	BeDirectoryFilePanel *fAdditionalDirectoryFilePanel;
 	BeDirectoryFilter *fAdditionalDirectoryFilter;
+
+	void SelectSomeGame(void)
+	{
+		bool found = false;
+		for(int i = 0; i < fGamesList->CountItems(); ++i)
+		{
+			BStringItem *stringItem = dynamic_cast<BStringItem *>(fGamesList->ItemAt(i));
+			BString textOfStringItem = stringItem->Text();
+			if(textOfStringItem.Compare(BeLauncherBase::GetSettings()->GetSettingsString(GAME_OPT)) == 0)
+			{
+				found = true;
+				fGamesList->Select(i);
+			}
+		}
+		if(!found)
+		{
+			fGamesList->Select(0);
+		}
+	}
+
+	void FillGamesList(void)
+	{
+		fGamesList->MakeEmpty();
+
+		entry_ref ref;
+		BEntry entry(BeLauncherBase::GetTextControl()->Text());
+		if(!entry.Exists() || !entry.IsDirectory())
+		{
+			fGamesList->AddItem(new BStringItem(L_ERROR_NO_VALVE_CATALOG));
+			return;
+		}
+		entry.GetRef(&ref);
+
+		BDirectory directory(&ref);
+		if(directory.InitCheck() != B_OK)
+		{
+			fGamesList->AddItem(new BStringItem(L_ERROR_NO_VALVE_CATALOG));
+			return;
+		}
+		directory.Rewind();
+		BStringList stringList;
+		while(directory.GetNextEntry(&entry, true) == B_OK)
+		{
+			if(entry.IsDirectory())
+			{
+				entry.GetRef(&ref);
+				stringList.Add(BString(ref.name));
+			}
+		}
+
+		if(stringList.HasString(VALVE_DIR_NAME))
+		{
+			for(int i = 0; i < stringList.CountStrings(); ++i)
+			{
+				fGamesList->AddItem(new BStringItem(stringList.StringAt(i)));
+			}
+		}
+		else
+		{
+			fGamesList->AddItem(new BStringItem(L_ERROR_NO_VALVE_CATALOG));
+		}
+	}
 
 	static const BString GetPathToPackageExtras(void)
 	{
@@ -125,6 +200,10 @@ protected:
 				bool value = static_cast<bool>(fCheckBoxOption->Value());
 				fAdditionalTextControl->SetEnabled(value);
 				fAdditionalBrowseButton->SetEnabled(value);
+				if(!value)
+				{
+					fAdditionalTextControl->SetText(BeLauncherBase::GetSettings()->GetSettingsString(MIRROR_PATH_OPT));
+				}
 				break;
 			}
 			case MSG_ADDITIONAL_BROWSE_BUTTON_CLICKED:
@@ -135,6 +214,29 @@ protected:
 			case MSG_ADDITIONAL_FILE_PANEL_FILE_SELECTED:
 			{
 				BeLauncherBase::DirectorySelected(fAdditionalDirectoryFilePanel, fAdditionalTextControl);
+				break;
+			}
+			case MSG_FILE_PANEL_FILE_SELECTED:
+			{
+				BeLauncherBase::MessageReceived(msg);
+				FillGamesList();
+				fSelectedGame = VALVE_DIR_NAME;
+				BeLauncherBase::GetSettings()->SetSettingsString(GAME_OPT, fSelectedGame);
+				SelectSomeGame();
+				break;
+			}
+			case MSG_SELECTED_SOME_GAME:
+			{
+				int32 selection = fGamesList->CurrentSelection();
+				if(selection < 0)
+				{
+					fSelectedGame = VALVE_DIR_NAME;
+				}
+				BStringItem *stringItem = dynamic_cast<BStringItem *>(fGamesList->ItemAt(selection));
+				if (stringItem != NULL)
+				{
+					fSelectedGame = stringItem->Text();
+				}
 				break;
 			}
 			default:
@@ -164,7 +266,15 @@ protected:
 			mirrorPath = GetPathToPackageExtras();
 		}
 		setenv(MIRROR_PATH_OPT, mirrorPath.String(), 1);
-		setenv(GAME_OPT, "bshift", 1);
+
+		// NOTE: Just an extra check.
+		// If it does not fit the condition then this is a error, reset it.
+		if((fSelectedGame.Length() > G_MAX_GAME_NAME_LENGTH) ||
+		   (fSelectedGame.Compare("") == 0))
+		{
+			fSelectedGame = VALVE_DIR_NAME;
+		}
+		setenv(GAME_OPT, fSelectedGame.String(), 1);
 
 		return BeLauncherBase::RunGameViaRoster(true);
 	}
@@ -190,8 +300,7 @@ protected:
 			fAdditionalTextControl->SetEnabled(static_cast<bool>(value));
 			fAdditionalBrowseButton->SetEnabled(static_cast<bool>(value));
 
-			const char *path = BeLauncherBase::GetSettings()->GetSettingsString(MIRROR_PATH_OPT);
-			fAdditionalTextControl->SetText(path);
+			fAdditionalTextControl->SetText(BeLauncherBase::GetSettings()->GetSettingsString(MIRROR_PATH_OPT));
 		}
 		return true;
 	}
@@ -203,6 +312,7 @@ protected:
 		value << fCheckBoxOption->Value();
 		BeLauncherBase::GetSettings()->SetSettingsString(S_CHECKBOX_OPTION, value);
 		BeLauncherBase::GetSettings()->SetSettingsString(MIRROR_PATH_OPT, fAdditionalTextControl->Text());
+		BeLauncherBase::GetSettings()->SetSettingsString(GAME_OPT, fSelectedGame);
 
 		BeLauncherBase::SaveSettings(def);
 	}
@@ -236,6 +346,12 @@ public:
 		BStringView *urlDescString = new BStringView(O_DATA_LINK_DESC, L_DATA_FILES_LINK_D);
 		BeUrlStringView *urlString = new BeUrlStringView(O_DATA_LINK, L_DATA_LINK);
 
+		fGamesList = new BListView();
+		fGamesList->SetExplicitSize(BSize(B_SIZE_UNSET, G_GAMES_LIST_HEIGHT));
+		fGamesList->SetSelectionMessage(new BMessage(MSG_SELECTED_SOME_GAME));
+		BScrollView *scrollView = new BScrollView(O_VIEW_SCROLL, fGamesList, B_WILL_DRAW | B_FRAME_EVENTS,
+		                                          false, true, B_PLAIN_BORDER);
+
 		BGroupLayout *boxLayout = BLayoutBuilder::Group<>(B_VERTICAL, B_USE_HALF_ITEM_SPACING)
 		                          .AddGroup(B_HORIZONTAL, 0.0f)
 		                              .Add(fCheckBoxOption)
@@ -245,6 +361,7 @@ public:
 		                              .Add(fAdditionalTextControl)
 		                              .Add(fAdditionalBrowseButton)
 		                          .End()
+		                          .Add(scrollView)
 		                          .AddGroup(B_HORIZONTAL, 0.0f)
 		                              .Add(urlDescString)
 		                              .Add(urlString)
@@ -261,6 +378,10 @@ public:
 
 		// NOTE: Be sure to call read settings function.
 		ReadSettings();
+
+		// NOTE: Fill Game List and select some Game then.
+		FillGamesList();
+		SelectSomeGame();
 	}
 
 	virtual
